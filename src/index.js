05a9dfab5efab696e45fc11c7eb96da56d499887
marmooo <marmooo@users.noreply.github.com>
2023-07-06 00:25:39 +0900

    improve dark-mode

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ loadConfig();
 
 function loadConfig() {
   if (localStorage.getItem("darkMode") == 1) {
-    document.documentElement.dataset.theme = "dark";
+    document.documentElement.setAttribute("data-bs-theme", "dark");
   }
   if (localStorage.getItem("voice") == 0) {
     document.getElementById("voiceOn").classList.add("d-none");
@@ -27,10 +27,10 @@ function loadConfig() {
 function toggleDarkMode() {
   if (localStorage.getItem("darkMode") == 1) {
     localStorage.setItem("darkMode", 0);
-    delete document.documentElement.dataset.theme;
+    document.documentElement.setAttribute("data-bs-theme", "light");
   } else {
     localStorage.setItem("darkMode", 1);
-    document.documentElement.dataset.theme = "dark";
+    document.documentElement.setAttribute("data-bs-theme", "dark");
   }
 }
 
